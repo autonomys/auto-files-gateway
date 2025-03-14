3035092d93f9e38ad2fb5199ea2a3d069020b784
Carlos Lostao <hastro1990@gmail.com>
2025-03-14 19:04:58 -0400

    fix some bugs and add tests for them

---
+++ services/file-retriever/jest.config.ts
@@ -0,0 +1,24 @@
+const { createDefaultEsmPreset } = require('ts-jest')
+
+module.exports = {
+  ...createDefaultEsmPreset(),
+  testMatch: ['**/__tests__/**/*.spec.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  coveragePathIgnorePatterns: [
+    './__tests__/utils/',
+    './node_modules/',
+    './migrations/',
+  ],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+}
--- services/file-retriever/src/utils/concurrency.ts
+++ services/file-retriever/src/utils/concurrency.ts
@@ -1,43 +1,78 @@
-import Queue from 'yocto-queue'
+import { sum } from './number'
 
 export type Job<T> = () => Promise<T>
 
 type ConcurrencyController = <T>(job: Job<T>, concurrency: number) => Promise<T>
 
+interface ScheduleTask<T> {
+  job: Job<T>
+  concurrency: number
+}
+
 export const multiRequestConcurrencyController = (
   maxConcurrency: number,
+  ensureOrder: boolean = false,
 ): ConcurrencyController => {
   if (maxConcurrency <= 0) {
     throw new Error('Max concurrency must be greater than 0')
   }
 
-  const queue = new Queue<() => Promise<unknown>>()
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const queue: ScheduleTask<any>[] = []
   let active = 0
 
-  const enqueue = <T>(task: Job<T>): Promise<T> => {
+  const enqueue = <T>(task: Job<T>, concurrency: number): Promise<T> => {
     return new Promise<T>((resolve, reject) => {
-      queue.enqueue(() => task().then(resolve).catch(reject))
+      queue.push({
+        job: () => task().then(resolve).catch(reject),
+        concurrency,
+      })
     })
+  }
+
+  const findExecutableTask = () => {
+    const index = queue.findIndex(
+      (e) => active + e.concurrency <= maxConcurrency,
+    )
+    const notMatched = index === -1
+    const notFirst = ensureOrder && index !== 0
+    if (notMatched || notFirst) {
+      return null
+    }
+
+    console.log(
+      `Found task with index ${index} and concurrency ${queue[index].concurrency}`,
+    )
+
+    return queue.splice(index, 1).at(0)
   }
 
   const manageJobFinalization = async (concurrency: number) => {
     active -= concurrency
-    if (queue.size < maxConcurrency) {
-      const task = queue.dequeue()
-      if (task) {
-        active += 1
-        task()
-      }
+    const executableTask = findExecutableTask()
+    if (executableTask) {
+      setImmediate(() =>
+        internalRunJob(executableTask.job, executableTask.concurrency),
+      )
     }
   }
 
-  const runJob = async <T>(job: Job<T>, concurrency: number): Promise<T> => {
-    if (active >= maxConcurrency) {
-      return enqueue(job)
-    }
-
+  const internalRunJob = async <T>(
+    job: Job<T>,
+    concurrency: number,
+  ): Promise<T> => {
     active += concurrency
     return job().finally(() => manageJobFinalization(concurrency))
+  }
+
+  const runJob = async <T>(job: Job<T>, concurrency: number): Promise<T> => {
+    const exceededMaxConcurrency = active + concurrency > maxConcurrency
+    const shouldRespectOrder = ensureOrder && queue.length > 0
+    if (exceededMaxConcurrency || shouldRespectOrder) {
+      return enqueue(job, concurrency)
+    }
+
+    return internalRunJob(job, concurrency)
   }
 
   return runJob
