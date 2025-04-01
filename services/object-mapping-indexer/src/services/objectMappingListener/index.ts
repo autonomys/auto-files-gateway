e4980a7673d12d94c2917f582402cfe831b57eb2
Carlos Lostao <hastro1990@gmail.com>
2025-03-31 19:01:19 -0700

    chore: remove old things and adding logs

--- services/object-mapping-indexer/src/services/objectMappingListener/index.ts
+++ services/object-mapping-indexer/src/services/objectMappingListener/index.ts
@@ -1,22 +1,8 @@
 import { logger } from '../../drivers/logger.js'
-import { createSubstrateEventListener } from '../../drivers/substrateEvents.js'
-import { ObjectMappingListEntrySchema } from '@auto-files/models'
 import { objectMappingUseCase } from '../../useCases/objectMapping.js'
 import { ObjectMappingListener } from './types.js'
 import { SubspaceObjectListenerAPI } from '@auto-files/rpc-apis'
 import { config } from '../../config.js'
-
-const events: Record<string, (event: unknown) => void> = {
-  subspace_subscribeObjectMappings: async (event: unknown) => {
-    const parsed = ObjectMappingListEntrySchema.safeParse(event)
-    if (!parsed.success) {
-      logger.error(`Invalid event: ${JSON.stringify(event)}`)
-      return
-    }
-    logger.debug(`Processing object mapping: ${JSON.stringify(parsed.data)}`)
-    await objectMappingUseCase.processObjectMapping(parsed.data)
-  },
-}
 
 export const createObjectMappingListener = (): ObjectMappingListener => {
   return {
@@ -25,6 +11,10 @@ export const createObjectMappingListener = (): ObjectMappingListener => {
         await client.api.subspace_subscribeObjectMappings()
 
         client.onNotification('subspace_object_mappings', async (event) => {
+          logger.info(
+            `Processing object mapping (blockNumber=${event.blockNumber})`,
+          )
+          logger.debug(`Object mapping: ${JSON.stringify(event)}`)
           await objectMappingUseCase.processObjectMapping(event)
         })
       }
