597a86d329c43477d208469f4a9c3a91902f3eb5
Carlos Lostao <hastro1990@gmail.com>
2025-04-04 12:46:34 -0700

    feat: refactor RPC server setup and configuration management

--- services/object-mapping-indexer/src/server.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config.js'
-import { server } from './rpc/server.js'
-import { config } from './config.js'
-import { logger } from './drivers/logger.js'
-
-server.listen(config.port, () => {
-  logger.info(`Server is running on port ${config.port}`)
-})
