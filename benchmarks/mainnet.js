41b1bcada59ffe43c3db632f7d4ff377cb2a4184
Carlos Lostao <hastro1990@gmail.com>
2025-04-11 15:57:15 -0700

    update: mainnet benchmarks

--- benchmarks/mainnet.js
+++ benchmarks/mainnet.js
@@ -1,26 +1,30 @@
 import { group } from 'k6'
-import { mainnetFiles } from './files/mainnet.js'
+import { bigFiles as mainnetBigFiles, mainnetFiles } from './files/mainnet.js'
 import { loadEnv } from './utils/loadEnv.js'
 import http from 'k6/http'
 
 loadEnv()
 
 export default async function () {
-  group('Concurrent Requests', function () {
-    return http.batch(
-      mainnetFiles.map((file) => ({
-        url: `${global.MAINNET_FILES_GATEWAY_URL}/files/${file}?api_key=${global.MAINNET_FILES_GATEWAY_API_KEY}`,
-      })),
-    )
-  })
+  if (__ENV.SMALL_FILES ?? true) {
+    group('small-files', function () {
+      return http.batch(
+        mainnetFiles.map((file) => ({
+          url: `${global.MAINNET_FILES_GATEWAY_URL}/files/${file}?api_key=${global.MAINNET_FILES_GATEWAY_API_KEY}`,
+        })),
+      )
+    })
+  }
 
-  group('Big files', function () {
-    return http.batch(
-      mainnetFiles.map((file) => ({
-        url: `${global.MAINNET_FILES_GATEWAY_URL}/files/${file}?api_key=${global.MAINNET_FILES_GATEWAY_API_KEY}`,
-      })),
-    )
-  })
+  if (__ENV.BIG_FILES ?? true) {
+    group('big-files', function () {
+      return http.batch(
+        mainnetBigFiles.map((file) => ({
+          url: `${global.MAINNET_FILES_GATEWAY_URL}/files/${file}?api_key=${global.MAINNET_FILES_GATEWAY_API_KEY}`,
+        })),
+      )
+    })
+  }
 }
 
 export const options = {
