cc2e15bbeb5d9485b7beea340dba95a5f508f5f6
Carlos Lostao <hastro1990@gmail.com>
2025-04-14 12:36:11 -0700

    update: envvars for avoid big files

--- benchmarks/mainnet.js
+++ benchmarks/mainnet.js
@@ -7,7 +7,9 @@ loadEnv()
 
 export default async function () {
   const originControl = __ENV.ORIGIN_CONTROL ?? 'allow-cache'
-  if (__ENV.SMALL_FILES ?? true) {
+  const runSmallFiles =
+    typeof __ENV.SMALL_FILES === 'string' ? __ENV.SMALL_FILES === 'true' : true
+  if (runSmallFiles) {
     group('small-files', function () {
       return http.batch(
         mainnetFiles.map((file) => ({
@@ -17,7 +19,9 @@ export default async function () {
     })
   }
 
-  if (__ENV.BIG_FILES ?? true) {
+  const runBigFiles =
+    typeof __ENV.BIG_FILES === 'string' ? __ENV.BIG_FILES === 'true' : true
+  if (runBigFiles) {
     group('big-files', function () {
       return http.batch(
         mainnetBigFiles.map((file) => ({
