d868b9bbd7a2399d702a3d983840b018fc261069
Carlos Lostao <hastro1990@gmail.com>
2025-05-06 16:55:50 +0200

    feat: add benchmarks for mainnet object mappings and single object mapping

---
+++ benchmarks/subspaceMainnet.js
@@ -0,0 +1,69 @@
+import http from 'k6/http'
+import { group } from 'k6'
+import { taurusFiles } from './files/taurus.js'
+import { loadEnv } from './utils/loadEnv.js'
+import { mainnetObjectMappings } from './objectMappings/mainnet.js'
+
+loadEnv()
+
+export default async function () {
+  const runBigBatch =
+    typeof __ENV.BIG_BATCH === 'string' ? __ENV.BIG_BATCH === 'true' : true
+  if (runBigBatch) {
+    group('Mainnet Object Mappings', function () {
+      return http.batch(
+        mainnetObjectMappings.map((objects) => ({
+          method: 'POST',
+          url: global.SUBSPACE_MAINNET_FILES_GATEWAY_URL,
+          params: {
+            timeout: '1h',
+          },
+          body: {
+            jsonrpc: '2.0',
+            method: 'subspace_fetchObject',
+            params: {
+              mappings: {
+                v0: {
+                  objects,
+                },
+              },
+            },
+            id: 1,
+          },
+        })),
+      )
+    })
+  }
+
+  const runSingleObjectMapping =
+    typeof __ENV.SINGLE_OBJECT_MAPPING === 'string'
+      ? __ENV.SINGLE_OBJECT_MAPPING === 'true'
+      : true
+  if (runSingleObjectMapping) {
+    group('Single Object Mapping', function () {
+      return http.batch(
+        singleObjectMapping.map((objects) => ({
+          method: 'POST',
+          url: global.SUBSPACE_MAINNET_FILES_GATEWAY_URL,
+          params: {
+            timeout: '1h',
+          },
+          body: {
+            jsonrpc: '2.0',
+            method: 'subspace_fetchObject',
+            params: {
+              mappings: {
+                v0: { objects },
+              },
+            },
+          },
+        })),
+      )
+    })
+  }
+}
+
+export const options = {
+  batchPerHost: taurusFiles.length,
+  batch: taurusFiles.length,
+}
