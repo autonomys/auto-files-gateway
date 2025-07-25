8197619337521ad83b66319c866b3e9610fd0787
Carlos Lostao <hastro1990@gmail.com>
2025-07-25 09:18:16 +0200

    feat: update dag indexer project description and configuration

--- services/dag-indexer/project.ts
+++ services/dag-indexer/project.ts
@@ -16,7 +16,7 @@ const project: SubstrateProject = {
   version: '0.0.1',
   name: 'dag-indexer',
   description:
-    'SubQuery indexer for indexing DAG nodes from Substrate blockchain',
+    'SubQuery indexer for indexing DAG nodes from Autonomys blockchain',
   runner: {
     node: {
       name: '@subql/node',
@@ -37,27 +37,17 @@ const project: SubstrateProject = {
   },
   network: {
     /* The genesis hash of the network (hash of block 0) */
-    chainId:
-      '0x66455a580aabff303720aa83adbe6c44502922251c03ba73686d5245da9e21bd',
+    chainId: process.env.CHAIN_ID ?? '',
 
     // Multiple endpoints for load balancing and fallback
-    endpoint: ['https://rpc-0.mainnet.subspace.network'],
-    // @ts-ignore
-    types: {
-      Solution: {
-        public_key: 'AccountId32',
-        reward_address: 'AccountId32',
-      },
-      SubPreDigest: {
-        slot: 'u64',
-        solution: 'Solution',
-      },
-    },
+    endpoint: process.env.RPC_ENDPOINTS?.split(',') ?? [],
   },
   dataSources: [
     {
       kind: SubstrateDatasourceKind.Runtime,
-      startBlock: 3167587,
+      startBlock: process.env.START_BLOCK
+        ? parseInt(process.env.START_BLOCK)
+        : 0,
       mapping: {
         file: './dist/index.js',
         handlers: [
